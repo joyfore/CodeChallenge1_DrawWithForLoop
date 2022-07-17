eae1896a15decc7570746ff9cdc668c507611479
joice <joydingforever@gmail.com>
2022-07-17 14:49:35 -0700

    Initial commit

---
+++ CodeChallenge1_DrawWithForLoop.java
@@ -0,0 +1,30 @@
+
+class CodeChallenge1_DrawWithForLoop {
+    public static void main(String[] args) {
+
+        /*
+
+         ************
+         ************
+         ************
+         ************
+         ************
+
+         */
+
+    /*
+    Print this box shape using a for loop.
+    Remember \n means next line.
+    */
+        String[] ILoveYou = {"************","************", "************", "************", "************"};
+
+        for (String L : ILoveYou) {
+            System.out.println("************");
+            System.out.println("************");
+            System.out.println("************");
+            System.out.println("************");
+            System.out.println("************");
+            break;
+        }
+    }
+}
